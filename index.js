17d931c0f7b267d8f767fdd12eec843b3a008358
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2024-01-02 23:54:05 +0500

    load data and to use it

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-require('./logger')
+const log = require('./logger')
+
+log('hey yo what dup')
--- logger.js
+++ logger.js
@@ -1,3 +1,7 @@
 const url = 'https://192.168.7.1/logger';
 
-module.export.url = url;
+function log(message) {
+    console.log(message);
+}
+
+module.exports = log;
